a44d4817c735f9f1fff916df52b69fb48ec541bc
gearsandcode <jesse@2ofb.com>
2022-03-02 17:40:02 -0800

    fix: matui stub layout work

--- apps/color-ramp-tool/src/app/app.tsx
+++ apps/color-ramp-tool/src/app/app.tsx
@@ -1,6 +1,12 @@
 import styled from '@emotion/styled';
 import ColorGrid from '../components/color-grid/color-grid';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const StyledApp = styled.div`
   // Your style here
@@ -10,11 +16,29 @@ export function App() {
   return (
     <Router>
       <StyledApp>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Color Ramp Tools
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route path="/">
             <ColorGrid/>
           </Route>
         </Switch>
+      </Box>
       </StyledApp>
     </Router>
   );
--- apps/color-ramp-tool/src/components/color-grid/color-grid.tsx
+++ apps/color-ramp-tool/src/components/color-grid/color-grid.tsx
@@ -1,41 +1,46 @@
-import { Route, Link } from 'react-router-dom';
-
-import styled from '@emotion/styled';
-
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import styled from "@emotion/styled";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 /* eslint-disable-next-line */
 export interface ColorGridProps {}
 
 const StyledColorGrid = styled.div`
-  color: pink;
+  // color: pink;
+  h1: {
+    padding: 10px 0;
+  }
 `;
 
 export function ColorGrid(props: ColorGridProps) {
   return (
     <StyledColorGrid>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h6" component="h1" sx={{ pt: 2 }}>
               Color Ramp Tool
             </Typography>
-          </Toolbar>
-        </AppBar>
+          </Grid>
+          <Grid item xs="auto">
+            <Button variant="text">Text</Button>
+          </Grid>
+          <Grid item xs="auto">
+            <Button variant="contained">Contained</Button>
+          </Grid>
+          <Grid item xs="auto">
+            <Button variant="outlined">Outlined</Button>
+          </Grid>
+        </Grid>
       </Box>
       {/* <Route
         path="/"
